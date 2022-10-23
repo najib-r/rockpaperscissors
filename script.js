4eccafdc5bc2f77404956a17d7498e77a43f6641
ysosrss <najib72645@gmail.com>
2022-10-23 19:08:23 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-const choices = ["rock", "paper", "scissors"];  
+const choices = ["Rock", "Paper", "Scissors"];  
 
 function getComputerChoice () {
 
@@ -10,4 +10,48 @@ function getComputerChoice () {
     return choices[number];
 }
 
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    //playerSelection = playerSelection.charAt(0).toUpperCase()+playerSelection.slice(playerSelection.length - 1);
+    switch (playerSelection) {
+        case "Rock":
+            if (computerSelection === "Paper") {
+                return "You Lose! Paper beats Rock";
+            }
+            else if (computerSelection === "Rock") {
+                return "Draw!";
+            }
+            else {
+                return "You Win! Rock beats Scissors";
+            }
+            break;
+        case "Scissors":
+            if (computerSelection === "Paper") {
+                return "You Win! Scissors beats Paper";
+            }
+            else if (computerSelection === "Scissors") {
+                return "Draw!";
+            }
+            else {
+                return "You Lose! Rock beats Scissors";
+            }
+            break;
+        case "Paper":
+            if (computerSelection === "Paper") {
+                return "Draw!";
+            }
+            else if (computerSelection === "Rock") {
+                return "You Win! Paper beats Rock";
+            }
+            else {
+                return "You Lose! Scissors beats Paper";
+            }
+            break;
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(`Player choice - ${playerSelection}`);
+console.log(`Computer choice - ${computerSelection}`);
+console.log(playRound(playerSelection, computerSelection));
